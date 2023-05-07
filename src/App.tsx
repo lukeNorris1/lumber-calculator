ad2d23c9c0b32e3855d1f6bf521e365efb634874
lukeNorris1 <lukenorris2@gmail.com>
2023-05-07 13:02:16 +1000

    Added working input form and adding to wood cut list

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,116 @@
-import { useState } from "react";
+import { MouseEvent, useState } from "react";
 import AddWoodCut from "./components/AddWoodCut";
 
-  type rect = {
-    start: {
-      x: number,
-      y: number,
-    },
-    radius: number,
-    width: number,
-    height: number,
+type rect = {
+  start: {
+    x: number;
+    y: number;
+  };
+  radius: number;
+  width: number;
+  height: number;
+};
 
-  }
-
-  type woodCut = {
-    width: number,
-    height: number,
-    thickness: number
-}
+type woodCut = {
+  width: number;
+  height: number;
+  thickness: number;
+};
 
 function App() {
-  const [woodCutList, setWoodCutList] = useState<woodCut[]>([])
+  console.log("re-render");
+  const [woodCutList, setWoodCutList] = useState<woodCut[]>([]);
+
+
+  const [error, setError] = useState("");
 
   const [rectParam, setRectParam] = useState<rect>({
-    start: {x: 100, y: 100},
+    start: { x: 0, y: 0 },
     radius: 100,
-    width: 2000,
-    height: 2000
-  })
+    width: 1000,
+    height: 1000,
+  });
 
-  function addWoodCut(){
-      setWoodCutList([...woodCutList, {width: 50, height: 50, thickness: 50}])
+  function addWoodCut(width: number, height: number, thickness: number) {
+    setWoodCutList([...woodCutList, { width: width, height: height, thickness: thickness }]);
+  }
+
+  function handleWoodForm(e: MouseEvent<HTMLButtonElement, MouseEvent> | any) {
+    e.preventDefault();
+    inputIsNumber(e.target[0].value, e.target[1].value, e.target[2].value) ? addWoodCut(e.target[0].value, e.target[1].value, e.target[2].value) : setError("invalid input, please only enter valid numbers")
+  }
+
+  function inputIsNumber(width: string, height: string, thickness: string) {
+    return (!isNaN(Number(width)) && !isNaN(Number(height)) && !isNaN(Number(thickness)));
+  }
+
+  function WoodForm() {
+    return (
+      <div>
+        <form
+          className="flex flex-col justify-center mt-5 border-solid border-2 border-red-400 [&>*]:py-2 px-4 items-center  [&>*]:[&>*]:px-2 text-black"
+          onSubmit={(e) => handleWoodForm(e)}
+        >
+          <div className="flex flex-col">
+            <div className="flex flex-col item  s-start">
+              <p>Width:</p>
+              <input
+                className="px-4 py-2 border rounded"
+                type="number"
+                defaultValue={0}
+                min={1}
+              />
+            </div>
+            <div className="flex flex-col items-start mt-4">
+              <p>Height:</p>
+              <input
+                className="px-4 py-2 border rounded"
+                type="number"
+                defaultValue={0}
+                min={1}
+
+
+              />
+            </div>
+            <div className="flex flex-col items-start mt-4">
+              <p>Thickness:</p>
+              <input
+                className="px-4 py-2 border rounded"
+                type="number"
+                defaultValue={0}
+                min={1}
+
+              />
+            </div>
+          </div>
+          <button className="border-solid border-2 border-gray-400 rounded-md p-3 mb-3">
+            {" "}
+            Add Cut
+          </button>
+        </form>
+      </div>
+    );
   }
 
   return (
     <div className="flex flex-col justify-center items-center">
-
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <AddWoodCut addtoCut={setwoodCutList}/>
+      <WoodForm />
 
-      <svg width={rectParam.width} height={rectParam.height} className="bg-red-100">
-        {/* <circle
-          cx={rectParam.start.x}
-          cy={rectParam.start.y}
-          r={rectParam.radius}
-          stroke="green"
-          stroke-width="4"
-          fill="yellow"
-        /> */}
-         <rect width="1000" height="100" x="500" y="500" fill="yellow" />
+      {woodCutList.map((e, index) => {
+        return (
+          <div key={index}>
+            Width: {e.width}, Height: {e.height},  Thickness: {e.thickness}
+          </div>
+        );
+      })}
+      <svg
+        width={rectParam.width + 100}
+        height={rectParam.height}
+        className="bg-red-100"
+      >
+        <rect width="1000" height="100" x="50" y="5" fill="yellow" />
+        <rect width="1000" height="100" x="50" y="105" fill="blue" />
       </svg>
     </div>
   );
