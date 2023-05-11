3d2608eeae551d1943caa1d86e2b1902c5cca4d7
lukeNorris1 <lukenorris2@gmail.com>
2023-05-11 11:28:48 +1000

    Added visible heights and widths on cuts as well as proper spacing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { MouseEvent, useEffect, useState } from "react";
-import AddWoodCut from "./components/AddWoodCut";
 
 type rect = {
   start: {
@@ -21,28 +20,43 @@ function App() {
   console.log("re-render");
   const [woodCutList, setWoodCutList] = useState<woodCut[]>([]);
   const [error, setError] = useState("");
-  const [nominalWidth, setnNominalWidth] = useState<number>(1)
-  const [nominalHeight, setnNominalHeight] = useState<number>(1)
-
+  const [nominalWidth, setnNominalWidth] = useState<number>(1);
+  const [nominalHeight, setnNominalHeight] = useState<number>(1);
+  let totalHeight = 0;
 
   useEffect(() => {
-    console.log(`wood cut list lenght: ${woodCutList.length}`)
-
-  }, [woodCutList])
-  
+    console.log(`wood cut list lenght: ${woodCutList.length}`);
+  }, [woodCutList]);
 
   function addWoodCut(width: number, height: number, thickness: number) {
-    setWoodCutList([...woodCutList, { width: width, height: height, thickness: thickness }]);
+    setWoodCutList([
+      ...woodCutList,
+      { width: width, height: height, thickness: thickness },
+    ]);
   }
 
   function handleWoodForm(e: MouseEvent<HTMLButtonElement, MouseEvent> | any) {
     e.preventDefault();
-    inputIsNumber(e.target[0].value, e.target[1].value, e.target[2].value) ? addWoodCut(e.target[0].value, e.target[1].value, e.target[2].value) : setError("invalid input, please only enter valid numbers")
+    inputIsNumber(e.target[0].value, e.target[1].value, e.target[2].value)
+      ? addWoodCut(
+          parseInt(e.target[0].value),
+          parseInt(e.target[1].value),
+          parseInt(e.target[2].value)
+        )
+      : setError("invalid input, please only enter valid numbers");
   }
 
   function inputIsNumber(width: string, height: string, thickness: string) {
-    return (!isNaN(Number(width)) && !isNaN(Number(height)) && !isNaN(Number(thickness)));
+    return (
+      !isNaN(Number(width)) &&
+      !isNaN(Number(height)) &&
+      !isNaN(Number(thickness))
+    );
   }
+
+  console.log(
+    `window width: ${top?.innerWidth}, window height: ${window.outerHeight}`
+  );
 
   function WoodForm() {
     return (
@@ -68,8 +82,6 @@ function App() {
                 type="number"
                 defaultValue={0}
                 min={1}
-
-
               />
             </div>
             <div className="flex flex-col items-start mt-4">
@@ -79,14 +91,21 @@ function App() {
                 type="number"
                 defaultValue={0}
                 min={1}
-
               />
             </div>
           </div>
-          <button className="border-solid border-2 border-gray-400 rounded-md p-3 mb-3">
-            {" "}
-            Add Cut
-          </button>
+          <div>
+            <button className="border-solid border-2 border-gray-400 rounded-md p-3 mb-3">
+              {" "}
+              Add Cut
+            </button>
+            <button
+              className="border-solid border-2 border-gray-400 rounded-md p-3 ml-5 mb-3"
+              onClick={() => setWoodCutList([])}
+            >
+              Clear
+            </button>
+          </div>
         </form>
       </div>
     );
@@ -100,20 +119,47 @@ function App() {
       {woodCutList.map((e, index) => {
         return (
           <div key={index}>
-            Width: {e.width}, Height: {e.height},  Thickness: {e.thickness}
+            Width: {e.width}, Height: {e.height}, Thickness: {e.thickness}
           </div>
         );
       })}
       <svg
-        width={(1000 * nominalWidth) + 100}
-        height={((woodCutList.length * 100) * nominalHeight) + 20}
+        width={1000 * nominalWidth + 100}
+        height={
+          woodCutList.reduce((total, current) => total + current.height, 0) *
+            nominalHeight +
+          60
+        }
         className="bg-red-100"
       >
         {woodCutList.map((cut, index) => {
-        return (
-          <rect width={cut.width} height={95} x="50" y={(index * 100) + 5} fill={index % 2 == 0 ? "red" : "green"} />
-        );
-      })}
+          totalHeight += cut.height;
+          return (
+            <>
+              <rect
+                width={cut.width}
+                height={cut.height}
+                x="50"
+                y={totalHeight - cut.height + 10 * index + 10}
+                fill={index % 2 == 0 ? "red" : "green"}
+              />
+              <text
+                x={cut.width / 2}
+                y={totalHeight - cut.height + 10 * index + 30}
+                className=""
+              >
+                {cut.width}
+              </text>
+              <text
+                x={55}
+                y={totalHeight - cut.height + 30 + cut.height / 2}
+                className=""
+              >
+                {cut.height}
+              </text>
+            </>
+          );
+        })}
       </svg>
     </div>
   );
